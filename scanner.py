26269f32117a798dbe2b0ccf014d3de02154c31f
HazelSharma <99499352+HazelSharmaCoderHZ@users.noreply.github.com>
2024-09-30 23:08:51 +0530

    Update scanner.py hz

--- scanner.py
+++ scanner.py
@@ -1,9 +1,9 @@
 import concurrent.futures
 from config import global_results, error_logs
-from utils import fetch_url, parse_ports, parse_directories,print_message
+from utils import fetch_url, parse_ports, parse_directories, print_message
+from reporting import save_results
 
 def perform_scan(args):
-    
     base_url = args.url  
     port = parse_ports(base_url)  
     directories = parse_directories(args)  
@@ -16,17 +16,15 @@ def perform_scan(args):
         full_url = f"{base_url}:{port}/{directory}"
         urls_to_scan.append(full_url)
     
-    for directory in directories:
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-            futures = {executor.submit(fetch_url, url, headers, args.ignorecertificate, args.timeout, verbose=args.verbose): url for url in urls_to_scan}
-            
-            for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                if result:  
-                    global_results.append(result)
+    # Using ThreadPoolExecutor to fetch URLs concurrently
+    with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+        futures = {executor.submit(fetch_url, url, headers, args.ignorecertificate, args.timeout, verbose=args.verbose): url for url in urls_to_scan}
+        
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:  
+                global_results.append(result)
 
     print_message("Scan completed", verbose=args.verbose)
     
-    from reporting import save_results
-    save_results(global_results, args.output)
+    save_results(global_results, args.output)  # Call save_results to save the results after the scan
